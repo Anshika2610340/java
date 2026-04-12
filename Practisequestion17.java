7eecdbaa0dd8be38310a487f5278d12ee8f6ac24
Anshika Agarwal <anshikaagarwal2610@gmail.com>
2026-04-12 01:53:52 -0700

    practise question 17

---
+++ Practisequestion17.java
@@ -0,0 +1,8 @@
+public class Practisequestion17 {
+    public static void main(String[] args) {
+        /*Write a Java program to test if a given string contains the specified sequence of char values. */
+        String str1="hello welcome to programming";
+        String str2="welcome";
+        System.out.println(str1.contains(str2));
+    }
+}
