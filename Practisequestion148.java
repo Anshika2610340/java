3d621756a9d8169075d7f80afeae11fef0a1f24d
Anshika Agarwal <anshikaagarwal2610@gmail.com>
2026-04-12 04:40:55 -0700

    practise question 148

---
+++ Practisequestion148.java
@@ -0,0 +1,19 @@
+class animal{
+    public void move(){
+        System.out.println("moving.....");
+    }
+}
+class cheetah extends animal{
+    @Override
+    public void move(){
+        System.out.println("cheetah moving.....");
+    }
+}
+public class Practisequestion148 {
+    public static void main(String[] args) {
+        /* Write a Java program to create a class called Animal with a method named move(). 
+        Create a subclass called Cheetah that overrides the move() method to run. */
+        cheetah c=new cheetah();
+        c.move();
+    }
+}
