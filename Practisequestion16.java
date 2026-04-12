6627008ff2b6b432c6cdcdbc3400e5bf03add5e0
Anshika Agarwal <anshikaagarwal2610@gmail.com>
2026-04-12 01:51:41 -0700

    practise question 16

---
+++ Practisequestion16.java
@@ -0,0 +1,8 @@
+public class Practisequestion16 {
+    public static void main(String[] args) {
+        /* Write a Java program to concatenate a given string to the end of another string.*/
+        String str1="this is";
+        String str2=" programming";
+        System.out.println(str1.concat(str2));
+    }
+}
