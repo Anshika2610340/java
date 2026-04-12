78fc952a2cd822ad8e7cf4655fea47367b68e9f3
Anshika Agarwal <anshikaagarwal2610@gmail.com>
2026-04-12 01:45:28 -0700

    practise question 12

---
+++ Practisequestion12.java
@@ -0,0 +1,9 @@
+public class Practisequestion12 {
+    public static void main(String[] args) {
+        /*Write a Java program to compute the floor division and floor modulus of the given dividend and divisor. */
+        int x=-2345;
+        int y=456;
+        System.out.println("floor division is:"+Math.floorDiv(x,y));
+        System.out.println("floor modulus is:"+Math.floorMod(x,y));
+    }
+}
