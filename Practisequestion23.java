f3dae03701624921b8d3ace7cf5d31abc8b55cc3
Anshika Agarwal <anshikaagarwal2610@gmail.com>
2026-04-12 02:03:46 -0700

    practise question 23

---
+++ Practisequestion23.java
@@ -0,0 +1,7 @@
+public class Practisequestion23 {
+    public static void main(String[] args) {
+        /*Write a Java program to replace a specified character with another character. */
+        String str="the quick brown fox jumps over the lazy dog";
+        System.out.println(str.replace("d","f"));
+    }
+}
