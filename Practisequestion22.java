c8b4381e0ed6ea1ee397fc6fa227f3f20106e029
Anshika Agarwal <anshikaagarwal2610@gmail.com>
2026-04-12 02:02:02 -0700

    practise question 22

---
+++ Practisequestion22.java
@@ -0,0 +1,36 @@
+public class Practisequestion22 {
+    public static void main(String[] args) {
+        /*Write a Java program to get the index of all the characters of the alphabet.
+        Sample string of all alphabet: "The quick brown fox jumps over the lazy dog." */
+        String str="The quick brown fox jumps over the lazy dog";
+        int a=str.indexOf("a");
+        int b=str.indexOf("b");
+        int c=str.indexOf("c");
+        int d=str.indexOf("d");
+        int e=str.indexOf("e");
+        int f=str.indexOf("f");
+        int g=str.indexOf("g");
+        int h=str.indexOf("h");
+        int i=str.indexOf("i");
+        int j=str.indexOf("j");
+        int k=str.indexOf("k");
+        int l=str.indexOf("l");
+        int m=str.indexOf("m");
+        int n=str.indexOf("n");
+        int o=str.indexOf("o");
+        int p=str.indexOf("p");
+        int q=str.indexOf("q");
+        int r=str.indexOf("r");
+        int s=str.indexOf("s");
+        int t=str.indexOf("t");
+        int u=str.indexOf("u");
+        int v=str.indexOf("v");
+        int w=str.indexOf("w");
+        int x=str.indexOf("x");
+        int y=str.indexOf("y");
+        int z=str.indexOf("z");
+        System.out.format("a\tb\tc\td\te\tf\tg\th\ti\tj\tk\tl\tm\tn\to\tp\tq\tr\ts\tt\tu\tv\tw\tx\ty\tz\n");
+        System.out.println("=====================================================================================================");
+        System.out.println(a+" "+b+" "+c+" "+d+" "+e+" "+f+" "+g+" "+h+" "+i+" "+j+" "+k+" "+l+" "+m+" "+n+" "+o+" "+p+" "+q+" "+r+" "+s+" "+t+" "+u+" "+v+" "+w+" "+x+" "+y+" "+z);
+    }
+}
