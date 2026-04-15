f2d96a9707df895d9e94fafc061dbbafbb22431c
Anshika Agarwal <anshikaagarwal2610@gmail.com>
2026-04-15 05:28:45 -0700

    practise question 150

---
+++ Practisequestion150.java
@@ -0,0 +1,36 @@
+class shape{
+    int dim1,dim2;
+    public shape(int dim1,int dim2){
+        this.dim1=dim1;
+        this.dim2=dim2;
+    }
+    public int getperimeter(){
+        int perimeter=2*(dim1+dim2);
+        return perimeter;
+    }
+    public int getarea(){
+        int area=dim1*dim2;
+        return area;
+    }
+}
+class circle extends shape{
+    public circle(int dim1,int dim2){
+        super(dim1,dim2);
+    }
+    @Override
+    public int getperimeter(){
+        return (int)(2*Math.PI*dim1);
+    }
+    public int getarea(){
+        return (int)(Math.PI*dim1*dim2);
+    }
+}
+public class Practisequestion150 {
+    public static void main(String[] args) {
+        /*Write a Java program to create a class called Shape with methods called getPerimeter() and getArea(). 
+        Create a subclass called Circle that overrides the getPerimeter() and getArea() methods to calculate the area and perimeter of a circle.*/
+        circle c=new circle(2,2);
+        System.out.println(c.getperimeter());
+        System.out.println(c.getarea());
+    }
+}
