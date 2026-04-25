b3f1aca4f50ae5649f0f30579a7671047081c5a7
Anshika Agarwal <anshikaagarwal2610@gmail.com>
2026-04-25 05:13:57 -0700

    "practise question 149"

--- Practisequestion149.java
+++ Practisequestion149.java
@@ -1,6 +1,6 @@
-class person{
+class person1{
     String firstname,lastname;
-    public person(String firstname,String lastname){
+    public person1(String firstname,String lastname){
         this.firstname=firstname;
         this.lastname=lastname;
     }
@@ -11,9 +11,9 @@ class person{
         return lastname;
     }
 }
-class employee extends person{
+class employee1 extends person1{
     String employeeid,jobtitle;
-    public employee(String firstname,String lastname,String employeeid,String jobtitle){
+    public employee1(String firstname,String lastname,String employeeid,String jobtitle){
         super(firstname,lastname);
         this.employeeid=employeeid;
         this.jobtitle=jobtitle;
@@ -30,7 +30,7 @@ public class Practisequestion149 {
     public static void main(String[] args) {
         /*Write a Java program to create a class known as Person with methods called getFirstName() and getLastName(). 
         Create a subclass called Employee that adds a new method named getEmployeeId() and overrides the getLastName() method to include the employee's job title */
-        employee e=new employee("anita","jaiswal","e102","hod");
+        employee1 e=new employee1("anita","jaiswal","e102","hod");
         System.out.println(e.getemployeeid());
         System.out.println(e.getfirstname());
         System.out.println(e.getlastname());
