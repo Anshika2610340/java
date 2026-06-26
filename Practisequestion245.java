37f9c3af2fc481cfc34c600a5d22b169db8231f0
Anshika Agarwal <anshikaagarwal2610@gmail.com>
2026-06-26 12:47:10 +0530

    "practise question 245"

---
+++ Practisequestion245.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Practisequestion245 {
+    public static void main(String[] args) {
+        /*Write a Java program to get the number of elements in a hash set.*/
+        HashSet<String> h1=new HashSet<>();
+        h1.add("purple");
+        h1.add("yellow");
+        h1.add("pink");
+        System.out.println("list:");
+        System.out.println(h1);
+        System.out.println("size of list:"+h1.size());
+    }
+}
