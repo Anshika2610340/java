023bc69faa8de6be3408e2bcb218dfee8377d11a
Anshika Agarwal <anshikaagarwal2610@gmail.com>
2026-06-26 12:53:10 +0530

    "practise question 246"

--- Practisequestion246.java
+++ Practisequestion246.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class Practisequestion246 {
     public static void main(String[] args) {
-        /*Write a Java program to empty an hash set.*/
+        /*Write a Java program to empty a hash set.*/
         HashSet<Integer> h1=new HashSet<>();
         h1.add(2);
         h1.add(3);
